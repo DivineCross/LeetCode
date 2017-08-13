dfa70f44d1d4252fb61c7c17f712945c04442a1f
DivineCross <DivineCross@users.noreply.github.com>
2017-08-13 19:12:38 +0800

    Add 'ListNode' and its extensions

--- CSharp/DataStructure.cs
+++ CSharp/DataStructure.cs
@@ -4,4 +4,38 @@ using System.Linq;
 
 namespace LeetCode.DataStructure
 {
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+
+        public static ListNode FromCollection(IEnumerable<int> collection)
+        {
+            if (collection == null)
+                throw new ArgumentException($"'{nameof(collection)}' can not be null.");
+
+            if (!collection.Any())
+                return null;
+
+            var head = new ListNode(collection.First());
+            var node = head;
+            foreach (var value in collection.Skip(1))
+            {
+                node.next = new ListNode(value);
+                node = node.next;
+            }
+
+            return head;
+        }
+    }
+
+    public static class ListNodeExtensions
+    {
+        public static IEnumerable<int> AsEnumerable(this ListNode head)
+        {
+            for (var node = head; node != null; node = node.next)
+                yield return node.val;
+        }
+    }
 }
