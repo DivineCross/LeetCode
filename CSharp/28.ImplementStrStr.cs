28a980e827884d0c3dc539c274283b815e9d3187
DivineCross <DivineCross@users.noreply.github.com>
2017-08-20 22:52:37 +0800

    Add '28.ImplementStrStr'

---
+++ CSharp/28.ImplementStrStr.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        // https://leetcode.com/problems/implement-strstr
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack == null || needle == null)
+                return -1;
+
+            for (int h = 0; h + needle.Length <= haystack.Length; ++h)
+                for (int n = 0; ; ++n)
+                    if (n == needle.Length)
+                        return h;
+                    else if (haystack[h + n] != needle[n])
+                        break;
+
+            return -1;
+        }
+    }
+
+    public partial class Test
+    {
+        [Theory]
+        [InlineData(new object[] { null, null, -1 })]
+        [InlineData(new object[] { null, "", -1 })]
+        [InlineData(new object[] { null, "A", -1 })]
+        [InlineData(new object[] { "", null, -1 })]
+        [InlineData(new object[] { "", "", 0 })]
+        [InlineData(new object[] { "", "A", -1 })]
+        [InlineData(new object[] { "A", null, -1 })]
+        [InlineData(new object[] { "A", "", 0 })]
+        [InlineData(new object[] { "A", "A", 0 })]
+        [InlineData(new object[] { "A", "Z", -1 })]
+        [InlineData(new object[] { "A", "AZ", -1 })]
+        [InlineData(new object[] { "AA", "A", 0 })]
+        [InlineData(new object[] { "ABC", "A", 0 })]
+        [InlineData(new object[] { "ABC", "B", 1 })]
+        [InlineData(new object[] { "ABC", "C", 2 })]
+        [InlineData(new object[] { "ABC", "AB", 0 })]
+        [InlineData(new object[] { "ABC", "BC", 1 })]
+        [InlineData(new object[] { "ABC", "AC", -1 })]
+        [InlineData(new object[] { "ABC", "ABC", 0 })]
+        [InlineData(new object[] { "ABC", "ABZ", -1 })]
+        [InlineData(new object[] { "ABC", "ABCD", -1 })]
+        [InlineData(new object[] { "ABCABC", "A", 0 })]
+        [InlineData(new object[] { "ABCABC", "B", 1 })]
+        [InlineData(new object[] { "ABCABC", "AB", 0 })]
+        [InlineData(new object[] { "ABCABC", "BC", 1 })]
+        [InlineData(new object[] { "ABCABC", "ABC", 0 })]
+        public void StrStr(string haystack, string needle, int expectedIndex)
+        {
+            Assert.Equal(expectedIndex, new Solution().StrStr(haystack, needle));
+            if (expectedIndex != -1)
+                Assert.Equal(haystack.Substring(expectedIndex, needle.Length), needle);
+        }
+    }
+}
