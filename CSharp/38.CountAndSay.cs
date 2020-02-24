3026fabdd48bf2f8723680e5d1125362c0c6f3f6
DivineCross <DivineCross@users.noreply.github.com>
2020-02-24 14:30:23 +0800

    Refactor '38.CountAndSay'
    - Refactor test data
    - Refactor statements with side effects
    - Rename variables

--- CSharp/38.CountAndSay.cs
+++ CSharp/38.CountAndSay.cs
@@ -8,7 +8,10 @@ namespace LeetCode
 {
     public partial class Solution
     {
-        private static readonly List<string> countAndSay = new List<string> { string.Empty, "1" };
+        private static readonly List<string> countAndSayStrs = new List<string> {
+            string.Empty,
+            "1",
+        };
 
         // https://leetcode.com/problems/count-and-say
         public string CountAndSay(int n)
@@ -16,9 +19,9 @@ namespace LeetCode
             if (n <= 0)
                 return string.Empty;
 
-            var lastString = countAndSay.Last();
+            var lastString = countAndSayStrs.Last();
             var sb = new StringBuilder();
-            for (var i = countAndSay.Count; i <= n; ++i)
+            for (var i = countAndSayStrs.Count; i <= n; ++i)
             {
                 sb.Clear();
                 var repeating = lastString[0];
@@ -35,32 +38,34 @@ namespace LeetCode
                     repeating = c;
                     count = 1;
                 }
-                countAndSay.Add(lastString = sb.ToString());
+
+                lastString = sb.ToString();
+                countAndSayStrs.Add(lastString);
             }
 
-            return countAndSay[n];
+            return countAndSayStrs[n];
         }
     }
 
     public partial class Test
     {
         [Theory]
-        [InlineData(new object[] { -99999, "" })]
-        [InlineData(new object[] { -1, "" })]
-        [InlineData(new object[] { 0, "" })]
-        [InlineData(new object[] { 1, "1" })]
-        [InlineData(new object[] { 2, "11" })]
-        [InlineData(new object[] { 3, "21" })]
-        [InlineData(new object[] { 4, "1211" })]
-        [InlineData(new object[] { 5, "111221" })]
-        [InlineData(new object[] { 6, "312211" })]
-        [InlineData(new object[] { 7, "13112221" })]
-        [InlineData(new object[] { 8, "1113213211" })]
-        [InlineData(new object[] { 9, "31131211131221" })]
-        [InlineData(new object[] { 10, "13211311123113112211" })]
-        [InlineData(new object[] { 11, "11131221133112132113212221" })]
-        [InlineData(new object[] { 12, "3113112221232112111312211312113211" })]
-        [InlineData(new object[] { 13, "1321132132111213122112311311222113111221131221" })]
+        [InlineData(-99999, "")]
+        [InlineData(-1, "")]
+        [InlineData(0, "")]
+        [InlineData(1, "1")]
+        [InlineData(2, "11")]
+        [InlineData(3, "21")]
+        [InlineData(4, "1211")]
+        [InlineData(5, "111221")]
+        [InlineData(6, "312211")]
+        [InlineData(7, "13112221")]
+        [InlineData(8, "1113213211")]
+        [InlineData(9, "31131211131221")]
+        [InlineData(10, "13211311123113112211")]
+        [InlineData(11, "11131221133112132113212221")]
+        [InlineData(12, "3113112221232112111312211312113211")]
+        [InlineData(13, "1321132132111213122112311311222113111221131221")]
         public void CountAndSay(int n, string expected)
         {
             Assert.Equal(expected, new Solution().CountAndSay(n));
