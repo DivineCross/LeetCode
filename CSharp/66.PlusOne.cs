1639af966a9e22b54e77b6cbf6c2e0b10c6a1465
DivineCross <DivineCross@users.noreply.github.com>
2020-02-24 15:12:34 +0800

    Refactor test data

--- CSharp/66.PlusOne.cs
+++ CSharp/66.PlusOne.cs
@@ -31,39 +31,39 @@ namespace LeetCode
     public partial class Test
     {
         [Theory]
-        [InlineData(new object[] {
+        [InlineData(
             null,
-            new int[0] })]
-        [InlineData(new object[] {
+            new int[0])]
+        [InlineData(
             new int[0],
-            new int[0] })]
-        [InlineData(new object[] {
+            new int[0])]
+        [InlineData(
             new[] { 0 },
-            new[] { 1 } })]
-        [InlineData(new object[] {
+            new[] { 1 })]
+        [InlineData(
             new[] { 1 },
-            new[] { 2 } })]
-        [InlineData(new object[] {
+            new[] { 2 })]
+        [InlineData(
             new[] {    9 },
-            new[] { 1, 0 } })]
-        [InlineData(new object[] {
+            new[] { 1, 0 })]
+        [InlineData(
             new[] { 1, 9 },
-            new[] { 2, 0 } })]
-        [InlineData(new object[] {
+            new[] { 2, 0 })]
+        [InlineData(
             new[] { 6, 9 },
-            new[] { 7, 0 } })]
-        [InlineData(new object[] {
+            new[] { 7, 0 })]
+        [InlineData(
             new[] {    9, 9 },
-            new[] { 1, 0, 0 } })]
-        [InlineData(new object[] {
+            new[] { 1, 0, 0 })]
+        [InlineData(
             new[] { 1, 8, 9 },
-            new[] { 1, 9, 0 } })]
-        [InlineData(new object[] {
+            new[] { 1, 9, 0 })]
+        [InlineData(
             new[] { 2, 1, 4, 7, 4, 8, 3, 6, 4, 7 },
-            new[] { 2, 1, 4, 7, 4, 8, 3, 6, 4, 8 } })]
-        [InlineData(new object[] {
+            new[] { 2, 1, 4, 7, 4, 8, 3, 6, 4, 8 })]
+        [InlineData(
             new[] {    9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
-            new[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } })]
+            new[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
         public void PlusOne(int[] digits, int[] expected)
         {
             Assert.Equal(expected, new Solution().PlusOne(digits));
